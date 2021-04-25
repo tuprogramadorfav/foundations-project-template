05fc375cae1537849775ca877f8088e98b215730
tuprogramadorfav <jromeroc@students.uni-mainz.de>
2021-04-24 20:31:56 -0500

    trying Migrations

--- great_project/__init__.py
+++ great_project/__init__.py
@@ -1,23 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-import os
 
 # configure Flask using environment variables
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASS')
 app = Flask(__name__)
+engine = create_engine('postgresql://programanding:Isaaceinstein21@127.0.0.1/CEBJJ')
+connection = engine.connect()
 app.config.from_pyfile("config.py")
 app.config['SECRET_KEY'] = '802023d0df4b9ee3a0341a80847a7e0b'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@127.0.0.1/CEBJJ"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://programanding:Isaaceinstein21@127.0.0.1/CEBJJ'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
 
 
 from great_project import website
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from great_project.website import app  # noqa: F401
+from great_project.__init__ import manager
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
